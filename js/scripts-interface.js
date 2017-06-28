d9c3076973fc702377a529bf3c3f849dcb1e93fb
Guest User <Guest@Epicodus-4F.local>
2017-06-28 10:38:52 -0700

    get API working, work on getting movies to display

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,6 +1,14 @@
-var ExampleModule = require('./../js/scripts.js').exampleModule;
+// var ExampleModule = require('./../js/scripts.js').exampleModule;
 
+var apiKey = "72601d02d4379c40ed70d6697cc032b1";
 
 $(document).ready(function() {
-
+  $('#movie-finder').click(function() {
+    var movie = $('#movie').val();
+    $('#movie').val('');
+    $.get('https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&query=' + movie, function(response) {
+      $('.show-movies').text('Similar titles to ' + movie + ' are: ' +  response.results[1,5].title);
+      console.log(response);
+    });
+  });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
-//business logic stuff for a particular module
-var ExampleModule = function(args) {
-  this.args = args; //to be replaced with constructor arguments
-};
-
-ExampleModule.prototype.examplePrototype = function() {
-  return 'this is an example prototype method';
-};
-
-exports.exampleModule = ExampleModule;
+// //business logic stuff for a particular module
+// var apiKey = require('./../.env').apiKey;
+//
+// var Movie = function() {
+// };
+//
+// Movie.prototype.getMovie = function(movieTitle, similarTitle) {
+//   $.get('api.themoviedb.org/3/' + movieTitle + '/550?apikey=' + apiKey).then(function(response) {
+//     similarTitle(movieTitle,)
+//   })
+// };
+//
+// exports.exampleModule = ExampleModule;
